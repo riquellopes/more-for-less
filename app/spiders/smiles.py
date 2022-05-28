122de1759c65889540d1e8d4003ad8154213bf28
Henrique Lopes <292844+riquellopes@users.noreply.github.com>
2022-05-27 23:00:07 -0300

    processed html

--- app/spiders/smiles.py
+++ app/spiders/smiles.py
@@ -1,8 +1,15 @@
 import scrapy
+from app.item import Item
 
 class Smiles(scrapy.Spider):
     name = 'smiles'
     allowed_domains = ['shoppingsmiles.com.br']
 
-    def parse(self, response, **kwargs):
-        return super().parse(response, **kwargs)
+    def parse(self, response):
+        boxes = response.css("span.slider-item.box-produto")
+
+        for box in boxes:
+            miles = box.css("span.num-ganhe::text").extract_first()
+            price = box.css("span.preco-por.preco-por-acumulo:last-child::text").extract_first()
+            url = box.css("a::attr(href)").extract_first()
+            yield Item(url=url, price=price, miles=miles)
--- app/spiders/tests/test_smile.py
+++ app/spiders/tests/test_smile.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from scrapy.http import HtmlResponse
 from app.spiders.smiles import Smiles
+from app.item import Item
 
 def read(name = "index"):
     script_dir = os.path.dirname(__file__)
@@ -15,3 +16,9 @@ def home():
 
 def test_should_get_item_jbl_box(home):
     smiles = Smiles(limit=1)
+    result = next(smiles.parse(home))
+
+    assert Item(
+        miles='3.330', 
+        price='1.109,90', 
+        url='/smiles/produto.jsf?f=8&p=2285673_8&n=Caixa+de+Som+JBL+Charge+5+Bluetooth+Port%C3%A1til+-+40W+com+Tweeter&a=true') == result
